2367395a95a3ec07b375f76e523b7f3c04967a4d
Matti Lehtimäki <matti.lehtimaki@gmail.com>
2017-02-09 21:48:19 +0200

    (hybris) Disable SELinux
    Change-Id: I0511b2a0de1b20996f4fb8e9f3569acb41e2cf0f

--- init/service.cpp
+++ init/service.cpp
@@ -57,6 +57,8 @@ using android::base::WriteStringToFile;
 namespace android {
 namespace init {
 
+// Disable SELinux
+/*
 static std::string ComputeContextFromExecutable(std::string& service_name,
                                                 const std::string& service_path) {
     std::string computed_context;
@@ -95,6 +97,7 @@ static std::string ComputeContextFromExecutable(std::string& service_name,
     }
     return computed_context;
 }
+*/
 
 static void SetUpPidNamespace(const std::string& service_name) {
     constexpr unsigned int kSafeFlags = MS_NODEV | MS_NOEXEC | MS_NOSUID;
@@ -277,11 +280,14 @@ void Service::SetProcessAttributes() {
             PLOG(FATAL) << "setuid failed for " << name_;
         }
     }
+// Disable SELinux
+/*
     if (!seclabel_.empty()) {
         if (setexeccon(seclabel_.c_str()) < 0) {
             PLOG(FATAL) << "cannot setexeccon('" << seclabel_ << "') for " << name_;
         }
     }
+*/
     if (priority_ != 0) {
         if (setpriority(PRIO_PROCESS, 0, priority_) != 0) {
             PLOG(FATAL) << "setpriority failed for " << name_;
@@ -724,6 +730,8 @@ bool Service::Start() {
     }
 
     std::string scon;
+// Disable SELinux
+/*
     if (!seclabel_.empty()) {
         scon = seclabel_;
     } else {
@@ -732,7 +740,7 @@ bool Service::Start() {
             return false;
         }
     }
-
+*/
     LOG(INFO) << "starting service '" << name_ << "'...";
 
     pid_t pid = -1;
